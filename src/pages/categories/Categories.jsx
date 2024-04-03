a97c2d0e03c0a3988497ea3882deeffc4c1f77c3
malakalbattire <malak.albattire@gmail.com>
2024-04-03 18:38:07 +0200

    categori

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Categories() {
   const [categories, setCategories] = useState([]);
