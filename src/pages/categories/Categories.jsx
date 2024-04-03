8d65eb3b8f3aede77b27e62feed9e0656dfc58bf
malakalbattire <malak.albattire@gmail.com>
2024-04-03 17:58:36 +0200

    categories rollup

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -3,12 +3,11 @@ import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
 export default function Categories() {
-  
   const [categories, setCategories] = useState([]);
   const getCategories = async () => {
     const { data } = await axios.get(`https://ecommerce-node4-five.vercel.app/categories/active?page=1&limit=20`);
     setCategories(data.categories);
-    console.log(data.categories)
+    console.log(data.categories);
   };
   useEffect(() => {
     getCategories();
